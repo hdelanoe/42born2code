f10e13e328aa848333024c0665fb11030eafb7c0
Hugo DELANOE <hdelanoe@e2r11p20.42.fr>
2017-05-05 12:13:21 +0200

    Libft

---
+++ Libft/ft_strjoin.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hdelanoe <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/04/12 12:06:03 by hdelanoe          #+#    #+#             */
+/*   Updated: 2017/05/02 21:05:48 by hdelanoe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+char	*ft_strjoin(const char *s1, const char *s2)
+{
+	char	*str;
+	size_t	len;
+
+	len = ft_strlen(s1) + ft_strlen(s2);
+	str = (char*)malloc(sizeof(char) * len + 1);
+	if (str == NULL)
+		return (NULL);
+	str = ft_strcpy(str, s1);
+	str = ft_strcat(str, s2);
+	return (str);
+}
